3c0bfba43602f73c6050dc0ec1bdba13da15544b
Nilay Basak <nilaybasak000@gmail.com>
2025-07-16 23:06:03 +0530

    Added Frontend Components For SignUp & SignIn

---
+++ Frontend/src/components/Auth.tsx
@@ -0,0 +1,127 @@
+import { useState, type ChangeEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { type SignupInput } from "@nilaybasak111/medium-common";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  // Sending post request to backend
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
+        postInputs
+      );
+      const jwt = response.data.jwt;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (error) {
+      alert("Error while sending request");
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className=" h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-400">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Signup" : "Signin"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-4">
+            {/* Email Inputbox in Signup */}
+              <LabelledInput
+                label="Email"
+                placeholder="nilaybasak@gmail.com"
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    email: e.target.value,
+                  });
+                }}
+              />
+            
+
+            {/* Username / Name Inputbox in Signup */}
+            {type === "signup" ?(<LabelledInput
+              label="Username"
+              placeholder="nilaybasak111"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  name: e.target.value,
+                });
+              }}
+            />):null}
+
+            {/* Password Inputbox in Signup */}
+            <LabelledInput
+              label="Password"
+              type={"password"}
+              placeholder="********"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button
+              onClick={sendRequest}
+              type="button"
+              className="w-full mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign Up" : "Sign In"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// SignUp Inputbox
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-semibold text-black text-bold pt-4">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
