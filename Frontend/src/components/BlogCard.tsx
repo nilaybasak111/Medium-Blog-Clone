404d8e19b624b5a3f5545952260f21756af0f920
Nilay Basak <nilaybasak000@gmail.com>
2025-07-17 22:38:08 +0530

    Added BlogCard Components for Blogs Route

--- Frontend/src/components/BlogCard.tsx
+++ Frontend/src/components/BlogCard.tsx
@@ -1,3 +1,58 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+interface BlogCardProps {
+  authorName: string;
+  title: string;
+  content: string;
+  publishedDate: string;
+}
+
+function Circle() {
+  return <div className="h-1 w-1 rounded-full bg-slate-500"></div>;
+}
+
+// Avatar Component for the Blog
+function AvatarComponent({ name }: { name: string }) {
+  // Take the first letter of the first two words
+  const initials = name
+    .split(" ")
+    .filter(Boolean)
+    .map((word) => word[0].toUpperCase())
+    .slice(0, 2)
+    .join("");
+  return (
+    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+      <span className="text-xs text-gray-600 dark:text-gray-300">
+        {initials}
+      </span>
+    </div>
+  );
+}
+
+export const BlogCard = ({
+  authorName,
+  title,
+  content,
+  publishedDate,
+}: BlogCardProps) => {
+  return (
+    <div className="p-4 border-b border-state-200 pb-2">
+      <div className="flex ">
+        <AvatarComponent name={authorName} />
+        <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+          {authorName}
+        </div>
+        <div className="flex justify-center flex-col pl-2">
+          <Circle />
+        </div>
+        <div className="flex justify-center flex-col pl-2 font-thin text-slate-500">
+          {publishedDate}
+        </div>
+      </div>
+      <div className="text-xl font-semibold pt-2">{title}</div>
+      {/* Add check here if content is greter than 100 then use ... */}
+      <div className="text-md font-thin">{content.slice(0, 100) + "...."}</div>
+      <div className="text-slate-500 text-sm font-thin pt-4">{`${Math.ceil(
+        content.length / 100
+      )} Minute(s) Take to Read`}</div>
+    </div>
+  );
 };
--- Frontend/src/pages/Blogs.tsx
+++ Frontend/src/pages/Blogs.tsx
@@ -1,3 +1,20 @@
+import { BlogCard } from "../components/BlogCard";
+
 export const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-xl">
+        <BlogCard
+          authorName={"Nilay Basak"}
+          title={
+            "First test blog frontend.First test blog frontend.First test blog frontend"
+          }
+          content={
+            "Loren Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Quia, quibusdam. Nisi, quidem. Quia, quibusdam. Nisi, quidem."
+          }
+          publishedDate={"2023-01-01"}
+        />
+      </div>
+    </div>
+  );
 };
